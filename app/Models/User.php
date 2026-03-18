4e5da360c16b9f036f326d2483bdafc0ec814363
MOUAINE-FATIHA <fatihamouaine1@gmail.com>
2026-03-18 11:00:00 +0000

    chore: configuration du fichier bootstrap/app.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,6 @@ class User extends Authenticatable
         'phone',
         'bio',
     ];
-
     protected $hidden = ['password', 'remember_token'];
 
     protected $casts = [
@@ -28,7 +27,6 @@ class User extends Authenticatable
         'password'          => 'hashed',
         'is_active'         => 'boolean',
     ];
-
     public function profilStagiaire()
     {
         return $this->hasOne(ProfilStagiaire::class);
