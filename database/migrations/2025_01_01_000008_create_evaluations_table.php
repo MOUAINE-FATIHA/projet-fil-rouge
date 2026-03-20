dc776f7de49053c0db5b327734aed4733ec2e34b
MOUAINE-FATIHA <fatihamouaine1@gmail.com>
2026-03-20 11:00:00 +0000

    création de la migration de la table evaluations

--- database/migrations/2025_01_01_000008_create_evaluations_table.php
+++ database/migrations/2025_01_01_000008_create_evaluations_table.php
@@ -5,8 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
-    public function up(): void
-    {
+    public function up(): void{
         Schema::create('evaluations', function (Blueprint $table) {
             $table->id();
             $table->foreignId('internship_id')->constrained()->onDelete('cascade');
@@ -23,9 +22,7 @@ return new class extends Migration
             $table->unique(['internship_id', 'evaluator_id', 'evaluator_type']);
         });
     }
-
-    public function down(): void
-    {
+    public function down(): void{
         Schema::dropIfExists('evaluations');
     }
 };
