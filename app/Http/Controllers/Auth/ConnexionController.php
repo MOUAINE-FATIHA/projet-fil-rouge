1988c7792a7326e68191dbe7f8652f69e108d65a
MOUAINE-FATIHA <fatihamouaine1@gmail.com>
2026-03-28 15:30:00 +0100

    configuration des routes auth avec middleware guest

--- app/Http/Controllers/Auth/ConnexionController.php
+++ app/Http/Controllers/Auth/ConnexionController.php
@@ -32,7 +32,6 @@ class ConnexionController extends Controller
         $request->session()->regenerate();
         return redirect()->intended(route('dashboard'));
     }
-
     public function deconnecter(Request $request)
     {
         Auth::logout();
